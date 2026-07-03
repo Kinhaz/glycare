5d4c3650bd3976cef4cf520bb03e641025a76233
Pedro Ferraz <164897264+Mg222324@users.noreply.github.com>
2026-07-03 12:16:47 +0000

    estrutura feita

---
+++ glycare/src/navigation/types.ts
@@ -0,0 +1,7 @@
+// =========================================
+// GlyCare — rotas do app
+// Adicione aqui o nome de cada nova tela
+// =========================================
+export type RootStackParamList = {
+};
+ 
